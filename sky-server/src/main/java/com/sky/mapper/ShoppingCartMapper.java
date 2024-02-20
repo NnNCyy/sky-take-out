3e90d52e8d59927fd7b1746e03e42c46292c6acb
NnNCyy <1029642530@qq.com>
2024-02-20 13:21:15 +0800

    订单相关代码

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
@@ -17,7 +18,7 @@ public interface ShoppingCartMapper {
     List<ShoppingCart> list(ShoppingCart shoppingCart);
 
     /**
-     * 根据id
+     * 根据id更新
      * @param shoppingCart
      */
     @Update("update shopping_cart set number = #{number} where id = #{id}")
@@ -30,5 +31,11 @@ public interface ShoppingCartMapper {
     @Insert("insert into shopping_cart (name, image, user_id, dish_id, setmeal_id, dish_flavor, number,amount, create_time) values " +
             "(#{name} ,#{image},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{number},#{amount},#{createTime})")
     void insert(ShoppingCart shoppingCart);
+
+    /**
+     * 根据userId删除
+     */
+    @Delete("delete from shopping_cart where user_id = #{userId}")
+    void deleteByUserId(Long userId);
 
 }
